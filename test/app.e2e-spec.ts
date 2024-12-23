04d999e77b522a0d126e469fd9d98039827642ba
Chetali Goyal <goyalchetali123@gmail.com>
2024-12-23 18:24:50 +0530

    adding e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,79 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('POST /auth/signup', () => {
+    it('should return a user on successful signup', async () => {
+      const authDto = {
+        email: 'johnDoe@example.com',
+        password: 'testpass123',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(authDto);
+
+      expect(response.status).toBe(403);
+      expect(response.body.email).toBe(undefined);
+      expect(response.body).not.toHaveProperty('hash');
+    });
+    it('should return a user on successful signup', async () => {
+      const authDto = {
+        email: 'janeDoe@example.com',
+        password: 'testpass123',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(authDto);
+
+      expect(response.status).toBe(403);
+      expect(response.body.email).toBe(undefined);
+      expect(response.body).not.toHaveProperty('hash');
+    });
+  });
+  describe('POST /auth/signin', () => {
+    it('should return a user on successful signin', async () => {
+      const authDto = {
+        email: 'janeDoe@example.com',
+        password: 'testpass123',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/auth/signin')
+        .send(authDto);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toHaveProperty('email', authDto.email);
+      expect(response.body).not.toHaveProperty('hash');
+    });
+  });
+
+  describe('PATCH /auth/updateUser', () => {
+    it('should update the user password and return the updated user', async () => {
+      const authDto = {
+        email: 'janeDoe@example.com',
+        password: 'NewPassword1234',
+      };
+
+      const response = await request(app.getHttpServer())
+        .patch('/auth/updateUser')
+        .send(authDto);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('email', authDto.email);
+      expect(response.body).not.toHaveProperty('hash'); // Ensure password hash isn't returned
+    });
+  });
+
+  describe('DELETE /auth/deleteUser', () => {
+    it('should delete the user and return a success message', async () => {
+      const email = 'janeDoe@example.com';
+
+      const response = await request(app.getHttpServer())
+        .delete('/auth/deleteUser')
+        .send({ email });
+
+      expect(response.status).toBe(200);
+    });
   });
 });
